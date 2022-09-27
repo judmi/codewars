a07bf8036bb951ebfaf606eeb111ae24fa360ae1
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2022-09-27 23:20:09 +0300

    upd

--- src/ShortestWord.java
+++ src/ShortestWord.java
@@ -1,15 +1,16 @@
 /*
+https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/java
+
 Simple, given a string of words, return the length of the shortest word(s).
 String will never be empty and you do not need to account for different data types.
 
-https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/java
+Example: bitcoin take over the world maybe who knows perhaps
  */
 
 public class ShortestWord {
     public static int findShort(String s) {
         String[] words = s.split(" ");
         String shortestWord = words[0];
-//        int shortestWordLength = 0;
 
         for(int i = 0; i < words.length; i++) {
             if (shortestWord.length() > words[i].length()) {
@@ -18,5 +19,4 @@ public class ShortestWord {
         }
         return shortestWord.length();
     }
-
 }
