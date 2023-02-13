6a8e98f7d0c0e1c78e1c6c69c2f8fed27e8b15d4
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-02-13 15:35:56 +0300

    upd

---
+++ src/sixkyu/MultipleThreeFive.java
@@ -0,0 +1,32 @@
+/*
+https://www.codewars.com/kata/514b92a657cdc65150000006/train/java
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+ */
+
+package sixkyu;
+
+public class MultipleThreeFive {
+
+    public static int solution(int number) {
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(16));
+    }
+
+}
