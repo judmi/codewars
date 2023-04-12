a0f95e2ff69dd5f99692c6ffcafe71614b3ee4c9
Julia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-04-12 18:24:50 +0300

    test (#8)

--- src/sixkyu/YouAreASquare.java
+++ src/sixkyu/YouAreASquare.java
@@ -31,7 +31,7 @@ public class YouAreASquare {
     public static boolean isSquare(int n) {
         int num = 0;
         int result = 0;
-
+        int test = 123;
         while (result <= n) {
             result = num * num;
             if (result == n) {
